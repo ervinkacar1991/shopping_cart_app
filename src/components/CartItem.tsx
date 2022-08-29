ed24d7625be79e4d744e81de28216b4bf44d1be4
ervinkacar1991 <kacarervin@gmail.com>
2022-08-29 13:39:38 +0200

    finished shopping cart app

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import storeItems from "../data/items.json";
-import { Stack } from "react-bootstrap";
+import { Stack, Button } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
 type CartItemProps = {
   id: number;
   quantity: number;
@@ -12,11 +13,32 @@ export function CartItem({ id, quantity }: CartItemProps) {
   if (item == null) return null;
 
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
       <img
         src={item.imgUrl}
         style={{ width: "125px", height: "75px", objectFit: "cover" }}
       />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              {quantity}x
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+      <div> {formatCurrency(item.price * quantity)}</div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </Button>
     </Stack>
   );
 }
